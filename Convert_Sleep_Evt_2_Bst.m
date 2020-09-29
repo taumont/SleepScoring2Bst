4f9ebdffee372365ef554c78edae3d23678a89f5
taumont <tomy.aumont@umontreal.ca>
2020-09-29 13:55:57 -0400

    [1] Code formatting for readability. [2] Improve some comments

--- Convert_Sleep_Evt_2_Bst.m
+++ Convert_Sleep_Evt_2_Bst.m
@@ -1,4 +1,5 @@
 % function Convert_Sleep_Evt_2_Bst()
+%CONVERT_SLEEP_EVT_2_BST - Convert RemLogic or CSV event files to MAT-file.
 % SYNOPSIS: Convert_Sleep_Evt_2_Bst()
 %
 %
@@ -6,9 +7,11 @@
 %   Recordings: All recordings must be in EDF/REC format.
 %   Events:     Supported event format are:
 %                   - CSV as exported by Gaetan's tool
-%                   - TXT as exported by RemLogic
+%                   - TXT as exported by RemLogic (*_Events.txt || *_Artifacts.txt). If Events AND
+%                       Artifacts are desired, they need to be in the same directory.
 %
 % REMARKS:
+%   IF used for SAM project, combine sleep socring and artifacts together.
 %   Column names in the event files must be previously verified not to contain accents.
 %   Recording year is assumed to be 20XX. To change it, see variable <CENTURY>
 %
@@ -34,90 +37,110 @@
 clear;clc;
 
 addpath('edfreadZip/')
-CENTURY = 2000; % Since years are save as only 2 digits in EDF files, add this number to it.
-SUBJ_ID_LENGTH = 8;
+CENTURY             = 2000; % Since years are save as only 2 digits in EDF files, add this number to it.
+SUBJ_ID_LENGTH      = 8;
+SUBJ_TO_ANALYZE     = {'SAM003n2','SAM030n1','SAM034n1','SAM037n2','SAM039n1','SAM041n1'};
+
 % ===== GET RECORDING FILES =====
 disp('Select recording directory')
-file_list = f_GetPath(dir(uigetdir('','Select recording directory')));
-recFiles = file_list(endsWith(file_list,{'.edf','.rec'},'IgnoreCase',true));
-recDir = fileparts(file_list{1});
+file_list       = f_GetPath(dir(uigetdir('','Select recording directory')));
+recFiles        = file_list(endsWith(file_list,{'.edf','.rec'},'IgnoreCase',true));
+recDir          = fileparts(file_list{1});
+
 % ===== GET MARKER FILES =====
 resp = f_AskYesNo({'  Use same directory for scoring files and recordings?\n\t--> "%s"',recDir});
 switch resp
     case 'yes'
-        mrkFiles = file_list(endsWith(file_list,{'.csv','txt'},'IgnoreCase',true));
-%         % Find which files are pairs
-%         iKeep = cellfun(@(c) find(strncmpi(c,mrkFiles,length(c)-4)),recFiles,'UniformOutput',false);
-%         recFiles = recFiles(~cellfun(@isempty,iKeep));
-%         mrkFiles = mrkFiles([iKeep{:}]);
+        mrkFiles    = file_list(endsWith(file_list,{'.csv','txt'},'IgnoreCase',true));
+        
     case 'no'
-        mrkFiles = f_GetPath(dir(uigetdir('','Select marker files directory')));
-        mrkFiles = mrkFiles(endsWith(mrkFiles,{'.csv','txt'},'IgnoreCase',true));        
+        mrkFiles    = f_GetPath(dir(uigetdir('','Select marker files directory')));
+        mrkFiles    = mrkFiles(endsWith(mrkFiles,{'.csv','txt'},'IgnoreCase',true));
 end
+
 % ===== GET RECORDING-MARKER FILES PAIRS =====
-[~,mrkFileName] = cellfun(@fileparts, mrkFiles, 'UniformOutput', false);
-[~,recFileName] = cellfun(@fileparts, recFiles, 'UniformOutput', false);
-iKeep = cellfun(@(c) find(strncmpi(c,mrkFileName,SUBJ_ID_LENGTH)),recFileName,'UniformOutput',false);
-recFiles = recFiles(~cellfun(@isempty,iKeep));
-mrkFiles =cellfun(@(c) cat(2,mrkFiles(c)),iKeep,'UniformOutput',false);
+[~,mrkFileName]     = cellfun(@fileparts, mrkFiles, 'UniformOutput', false);
+[~,recFileName]     = cellfun(@fileparts, recFiles, 'UniformOutput', false);
+iKeep               = cellfun(@(c) find(strncmpi(c,mrkFileName,SUBJ_ID_LENGTH)),recFileName,'UniformOutput',false);
+
+recFiles            = recFiles(~cellfun(@isempty,iKeep));
+mrkFiles            = cellfun(@(c) cat(2,mrkFiles(c)),iKeep,'UniformOutput',false);
+
+% Select only subject to process
+if ~isempty(SUBJ_TO_ANALYZE)
+    recFiles    = recFiles(contains(recFiles,SUBJ_TO_ANALYZE));
+    mrkFiles    = cellfun(@(c) c(contains(c,SUBJ_TO_ANALYZE)),mrkFiles,'UniformOutput',false);
+end
+
+mrkFiles            = mrkFiles(~cellfun(@isempty, mrkFiles));
 
 % Get subject names
-[~,fn,~] = cellfun(@fileparts,recFiles,'UniformOutput',false);
-sId = cellfun(@(n)n{1},cellfun(@(c)strsplit(c,'_'),fn,'UniformOutput',false),'UniformOutput',false);
+[~,fn,~]            = cellfun(@fileparts,recFiles,'UniformOutput',false);
+sId                 = cellfun(@(n)n{1},cellfun(@(c)strsplit(c,'_'),fn,'UniformOutput',false),'UniformOutput',false);
 
-% recFiles=recFiles([20,22,23,26,27]); mrkFiles=mrkFiles([20,22,23,26,27]); sId=sId([20,22,23,26,27]);
 % ===== PROCEED ON ONE FILE PAIR AT A TIME =====
 for iSubj = 1:length(recFiles)
     OVERWRITE = true; % Overwrite marker file if already exist
     f_PrintSubjectHeader(sId{iSubj});
-    for iFile = 1:length(mrkFiles{iSubj})
+    for iFile = 1:length(mrkFiles{iSubj}) % normally 2 files, sleep scoring + artifacts
         fprintf('Processing file: %s\n',mrkFiles{iSubj}{iFile})
         % Read event file
         if endsWith(mrkFiles{iSubj}{iFile},'csv','IgnoreCase',true)
-            cEvts = readcell(mrkFiles{iSubj}{iFile});
-            [t_col,e_col,d_col] = sf_Get_Evt_Related_Columns(cEvts(1,:),'harmonie');
+            cEvts                   = readcell(mrkFiles{iSubj}{iFile});
+            [t_col,e_col,d_col]     = sf_Get_Evt_Related_Columns(cEvts(1,:),'harmonie');
+            
         elseif endsWith(mrkFiles{iSubj}{iFile},'txt','IgnoreCase',true)
-            tEvts = readtable(mrkFiles{iSubj}{iFile});
-            colNames = tEvts.Properties.VariableNames;
-            cEvts = [colNames; table2cell(tEvts)];
-            [t_col,e_col,d_col] = sf_Get_Evt_Related_Columns(cEvts(1,:),'remlogic');
+            tEvts                   = readtable(mrkFiles{iSubj}{iFile});
+            colNames                = tEvts.Properties.VariableNames;
+            cEvts                   = [colNames; table2cell(tEvts)];
+            [t_col,e_col,d_col]     = sf_Get_Evt_Related_Columns(cEvts(1,:),'remlogic');
+            
         end
         if isempty(t_col) || isempty(e_col) || isempty(d_col)
             fprintf('WARNING: Skipping this run !!!\n')
             continue
         end
         % Get recording date/time
-        hdr = edfread(recFiles{iSubj});
-        dtV = str2double(strsplit([hdr.startdate '.' hdr.starttime],'.'));
-        recStart = datetime([CENTURY+dtV(3),dtV(2),dtV(1),dtV(4:end)]);
+        hdr         = edfread(recFiles{iSubj});
+        dtV         = str2double(strsplit([hdr.startdate '.' hdr.starttime],'.'));
+        recStart    = datetime([CENTURY+dtV(3),dtV(2),dtV(1),dtV(4:end)]);
         % Convert all events to Brainstorm event structure array
         if contains(recFiles{iSubj},'SAM028n1','IgnoreCase',true)
             % ===== FIX FOR SAM028 NIGHT 1 (manual export induce a time lag) =====
-            evt_time = cellfun(@(c) {c-(hours(1)+minutes(42)+seconds(30))},cEvts(2:end,t_col));
+            evt_time    = cellfun(@(c) {c-(hours(1)+minutes(42)+seconds(30))},cEvts(2:end,t_col));
+            
         elseif contains(recFiles{iSubj},'SAM028n2','IgnoreCase',true)
             % ===== FIX FOR SAM028 NIGHT 2 (manual export induce a time lag) =====
-            evt_time = cellfun(@(c) {c-(hours(1)+minutes(27)+seconds(45))},cEvts(2:end,t_col));
+            evt_time    = cellfun(@(c) {c-(hours(1)+minutes(27)+seconds(45))},cEvts(2:end,t_col));
+            
         else
-            evt_time = cEvts(2:end,t_col);
+            evt_time    = cEvts(2:end,t_col);
+            
         end
-        events = f_Convert_Evt_2_Bst([evt_time,cEvts(2:end,[e_col,d_col])],recStart);
+        events      = f_Convert_Evt_2_Bst([evt_time,cEvts(2:end,[e_col,d_col])],recStart);
+        
         % Makes sure no events are outside of recording duration. (Ex.: SAM015n2)
         for iEvt = 1:length(events)
-            events(iEvt).times(events(iEvt).times>hdr.records) = hdr.records;
+            if events(iEvt).times>(hdr.records*hdr.duration)
+                disp('Found some')
+            end
+            events(iEvt).times(events(iEvt).times>(hdr.records*hdr.duration)) = hdr.records;
         end
+        
+        % Ignore sleep stage scoring in artifact file
         if contains(mrkFiles{iSubj}{iFile},{'artifact'},'IgnoreCase',true)
-            events = events(~contains({events.label},{'R','W','N1','N2','N3','N/A'}));
+            events      = events(~contains({events.label},{'R','W','N1','N2','N3','N/A'}));
         end
         if ~isempty(events)
-            [fPath,fn,~] = fileparts(recFiles{iSubj});
-            evtFileName = fullfile(fPath,[fn '_Bst_Events']);
+            [fPath,fn,~]    = fileparts(recFiles{iSubj});
+            evtFileName     = fullfile(fPath,[fn '_Bst_Events']);
             if exist([evtFileName '.mat'],'file') && ~OVERWRITE
-                sEvt = load(evtFileName);
-                events = [sEvt.events,events];
+                sEvt        = load(evtFileName);
+                events      = [sEvt.events,events];
             end
             fprintf('Saving event file: %s\n',evtFileName)
             save(evtFileName,'-v6','events');
-            OVERWRITE = false;
+            OVERWRITE       = false;
         end
     end
 end
